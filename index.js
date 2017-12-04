f0a041974a76930f88f490e2dca18435861574d7
vivek87799 <daredevil.vivek@gmail.com>
2017-12-04 20:25:50 +0100

    added a clent chat window and created sockets

--- index.js
+++ index.js
@@ -13,7 +13,19 @@ var server = app.listen(9001,function(){
 app.use(express.static('view'));
 
 var io = socket(server);
-//listens for connection
+//listens for connection or calls from client
 io.on('connection',function(socket){
     console.log('connecting');
+    socket.on('cmessage',function(data){
+        //Now the received message is sent to all the connected clients or all the open sockets
+        //forwarding response to all the sockets
+        console.log(socket.id)
+        console.log(data);
+        io.sockets.emit('cmessage',data)
+    })
+    socket.on('getchatid',function(id){
+        console.log(id)
+        console.log(socket.id)
+        io.sockets.emit('getchatid',socket.id)
+    })
 });
--- view/webchat.js
+++ view/webchat.js
@@ -1,1 +1,35 @@
 var socket = io.connect('http://localhost:9001');
+//Storing the text in dom
+var message = document.getElementById('messagearea');
+var chatarea = document.getElementById('chatarea');
+var chtid = document.getElementById('chatid')
+var btn = document.getElementById('sendmessage');
+
+console.log('test socket id on init')
+socket.emit('getchatid');
+console.log(socket.id);
+document.getElementById("chatid").value = socket.id;
+
+
+// Listener to listen when a message is being sent
+// It forwards or emits the message to the server
+btn.addEventListener('click',function(){
+    document.getElementById("chatid").value = socket.id;    
+    socket.emit('cmessage',{
+        message: message.value,
+        chtid: socket.id
+
+    });
+});
+
+//listens to the port for new message
+socket.on('chatid',function(id){
+    console.log(data);
+    document.getElementById("chatid").value = id;
+})
+
+//listens to the port for new message
+socket.on('cmessage',function(data){
+    console.log(data);
+    chatarea.innerHTML += '<p>' + data.chtid + data.message + '<p>'; 
+})
