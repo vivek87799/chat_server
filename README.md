4c2cdd457c0bacabc23cb6cacf0952f2f25f700c
vivek87799 <daredevil.vivek@gmail.com>
2017-12-04 19:03:16 +0100

    adding the view for chat

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A simple chat server
 # Optional to install nodemon to auto refresh the server 
 npm install nodemon --save
+# using socket.io for communication bidirectional
+npm install socket.io --save
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 //Express is a nodejs framework
-var express = require('express')
+var express = require('express');
+var socket = require('socket.io');
 var app = express();
 
 // A call back function to listen and get the message
-var server = app.listen(9000,function(){
-    console.log('listening at 9000')
+var server = app.listen(9001,function(){
+    console.log('listening at 9001');
 });
 
-app.use(express.static('view'))
+
+//Redirecting the request to the views
+app.use(express.static('view'));
+
+var io = socket(server);
+//listens for connection
+io.on('connection',function(socket){
+    console.log('connecting');
+});
