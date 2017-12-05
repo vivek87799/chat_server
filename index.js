c7d2989d2bbef93386d3c72cf2c9ffe50dacedde
vivek87799 <daredevil.vivek@gmail.com>
2017-12-05 18:55:53 +0100

    chats history is persisted as JSON

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 // Express is a nodejs framework
 var express = require('express');
 var fs = require('fs')
-// Reading the chat history from txt file in sync so that it waits for the chat history to get loaded first
 var chathistory;
-chathistory = fs.readFileSync('db/chathistory.json');
 var socket = require('socket.io');
 var app = express();
+var newmessage = {};
+var messageid = 0;
 
-console.log(chathistory);
-
+// Reading the chat history from txt file in sync so that it waits for the chat history to get loaded first
+chathistory = JSON.parse(fs.readFileSync('db/chathistory.json'));
 // A call back function to listen and get the message
 var server = app.listen(9001,function(){
     console.log('listening at 9001');
@@ -18,17 +18,20 @@ var server = app.listen(9001,function(){
 
 //Redirecting the request to the views
 app.use(express.static('view'));
-
 var io = socket(server);
+
 //listens for connection or calls from client
 io.on('connection',function(socket){
     socket.on('cmessage',function(data){
-        console.log(chathistory);
-        console.log('adding new')
-        chathistory = chathistory + JSON.stringify(data, null, 2);
-        console.log(chathistory);        
-        
-        fs.writeFile('db/chathistory.json',chathistory,finished);
+        try{
+            messageid = chathistory[chathistory.length - 1].messageid + 1;            
+        }catch(err){
+            messageid = 0;
+        }
+        newmessage["messageid"] = messageid;
+        newmessage["message"] = data;
+        chathistory.push(newmessage);
+        fs.writeFile('db/chathistory.json',JSON.stringify(chathistory, null, 2),finished);
 
         function finished(){
             console.log('message written to file');
@@ -38,7 +41,14 @@ io.on('connection',function(socket){
         //forwarding response to all the sockets
         io.sockets.emit('cmessage',data)
     })
+
+    // sends the unique chat id for a client
     socket.on('getchatid',function(id){
         io.sockets.emit('getchatid',socket.id)
+    })
+
+    // sending the chat history at init
+    socket.on('chathistory',function(){
+        io.sockets.emit('chathistory',chathistory)
     })
 });
--- view/webchat.js
+++ view/webchat.js
@@ -1,6 +1,6 @@
 var socket = io.connect('http://localhost:9001');
 //Storing the text in dom
-var message = document.getElementById('messagearea');
+var messagearea = document.getElementById('messagearea');
 var chatarea = document.getElementById('chatarea');
 var chatid = document.getElementById('chatid')
 var btn = document.getElementById('sendmessage');
@@ -8,7 +8,8 @@ var cid;
 
 // To get unique id for the chat
 window.onload = function(){
-    socket.emit('getchatid');    
+    socket.emit('getchatid');        
+    socket.emit('chathistory');   
 };
 window.onfocus = function(){
     cid = socket.id.slice(-4);
@@ -18,11 +19,17 @@ window.onfocus = function(){
 // Listener to listen when a message is being sent
 // It forwards or emits the message to the server
 btn.addEventListener('click',function(){
+    // Validating for empty string
+    if(messagearea.value.trim() == ""){
+        alert('Please type your message');
+        return;
+    }
     socket.emit('cmessage',{
         chatid: socket.id.slice(-4),        
-        message: message.value
+        message: messagearea.value
 
     });
+    
 });
 
 //listens to the port to get the generated socket id
@@ -32,9 +39,18 @@ socket.on('getchatid',function(id){
     document.getElementById("chatid").value = cid;
 })
 
+//listens to the port to get the generated socket id
+socket.on('chathistory',function(chathistory){
+    chatarea.innerHTML = "";     
+    for (var i = 0; i<chathistory.length-1;i++){
+        chatarea.innerHTML += '<p>' + chathistory[i].message.chatid + ':' + chathistory[i].message.message + '<p>';
+    }  
+})
+
 //listens to the port for new message
 socket.on('cmessage',function(data){
     console.log(data);
-    console.log(socket.id)
+    console.log(socket.id);
+    messagearea.innerHTML.value = "";
     chatarea.innerHTML += '<p>' + data.chatid + ':' + data.message + '<p>'; 
 })
